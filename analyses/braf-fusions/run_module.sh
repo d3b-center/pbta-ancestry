9f9df8fe62fd7e03b234957da88ad3ff70b14676
Ryan Corbett <corbettr@chop.edu>
2024-01-24 16:24:49 -0500

    add README, wrapper script

---
+++ analyses/braf-fusions/run_module.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+# perform exon annotation of BRAF fusion breakpoints
+Rscript -e "rmarkdown::render('01-get-breakpoint-exons.Rmd')"
+
+# Breakpoint distribution analysis by predicted ancestry
+Rscript -e "rmarkdown::render('02-fusion-breakpoints-by-ancestry.Rmd')"
+
