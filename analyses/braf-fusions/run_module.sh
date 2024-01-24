67f7ccd756cf8388bf61e6bc00767289cb7af7e3
Ryan Corbett <corbettr@chop.edu>
2024-01-24 17:15:21 -0500

    update README, wrapper script

--- analyses/braf-fusions/run_module.sh
+++ analyses/braf-fusions/run_module.sh
@@ -9,3 +9,5 @@ Rscript -e "rmarkdown::render('01-get-breakpoint-exons.Rmd')"
 # Breakpoint distribution analysis by predicted ancestry
 Rscript -e "rmarkdown::render('02-fusion-breakpoints-by-ancestry.Rmd')"
 
+# Survival by breakpoint type
+Rscript -e "rmarkdown::render('03-survival-by-braf-breakpoints.Rmd')"
